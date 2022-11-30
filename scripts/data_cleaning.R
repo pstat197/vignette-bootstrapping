68513e07b183fe58f1ed19742c21d0e0c3d749bf
JayyyyLee <97768904+JayyyyLee@users.noreply.github.com>
2022-11-30 15:39:31 -0800

    regression

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -1,13 +1,15 @@
 library(tidyverse)
 library(dplyr)
 
+
 preprocessing = function(n){
   
   data=read.csv("data/raw/runs.csv")
-  
+  # randomly select n rows from the original dataset
   randomNum = sample(1:nrow(data), n)
   result =data %>% 
-    select(c(race_id,won,horse_age,horse_rating, horse_country,horse_type,
+    # only contains numerical predictor
+    select(c(race_id,won,horse_age,horse_rating,
              declared_weight, actual_weight,draw)) %>%
     mutate(r = c(1:nrow(data)))%>%
     mutate(won=factor(won))%>%
